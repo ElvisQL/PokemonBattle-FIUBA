5f52ab2145734acf62d4754c92445bb6dc748163
ElvisQL <quispeelvis7@gmail.com>
2023-11-09 12:12:04 -0300

    fix menu

--- src/main/java/org/fiuba/algoritmos3/PokemonApp.java
+++ src/main/java/org/fiuba/algoritmos3/PokemonApp.java
@@ -3,20 +3,25 @@ package org.fiuba.algoritmos3;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.media.MediaView;
+
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 
-import java.net.URL;
+
 
 
 public class PokemonApp extends Application {
 
+
     @Override
     public void start(Stage stage) throws Exception {
+
         FXMLLoader fxmlLoader = new FXMLLoader(PokemonApp.class.getResource("menu-view.fxml"));
-        MenuController menuController = fxmlLoader.getController();
+        System.out.println(getClass().getResource("menu-view.fxml"));
         Parent root = fxmlLoader.load();
+
+        MenuController menuController = fxmlLoader.getController();
+
         Scene scene = new Scene(root, 750, 700);
         stage.setTitle("Hello!");
         stage.setResizable(false);
