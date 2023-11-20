9f1c96f118ec898259b0edf70cfb49b4c4ec81c8
tcorzo <tcorzo@fi.uba.ar>
2023-11-20 15:43:53 -0300

    Start menu & player setup improvements

--- src/main/java/org/fiuba/algoritmos3/ui/controller/BaseController.java
+++ src/main/java/org/fiuba/algoritmos3/ui/controller/BaseController.java
@@ -1,8 +1,28 @@
 package org.fiuba.algoritmos3.ui.controller;
 
+import javafx.event.Event;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.fiuba.algoritmos3.GameAPI;
+import org.fiuba.algoritmos3.PokemonApp;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Optional;
 
 public abstract class BaseController implements Initializable {
     protected GameAPI gameAPI;
+
+    protected void changeScene(Event e, URL url) throws IOException {
+        FXMLLoader startMenuFXML = new FXMLLoader(url);
+        Scene scene = new Scene(startMenuFXML.load(), 1024, 1024);
+        Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+    }
+    protected URL getResource(String name) {
+        return PokemonApp.class.getResource(name);
+    }
 }
