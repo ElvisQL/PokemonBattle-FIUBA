bfa917b0a31eeeb8d41491986a42a3cc15e255ad
Pablo Gonzalez <pdgonzalez@fi.uba.ar>
2023-11-22 03:23:23 -0300

    pokemon choice roto xd

--- src/main/java/org/fiuba/algoritmos3/controller/ChoseGameMoveController.java
+++ src/main/java/org/fiuba/algoritmos3/controller/ChoseGameMoveController.java
@@ -1,13 +1,23 @@
 package org.fiuba.algoritmos3.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import org.fiuba.algoritmos3.view.chooseGameMove.PokemonView;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ChoseGameMoveController extends BaseController {
     public VBox pokemonsSplitPane;
+    @FXML
+    private Button changePokemon;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -17,5 +27,15 @@ public class ChoseGameMoveController extends BaseController {
 
         PokemonView currentPokemonView = new PokemonView(gameAPI.currentPlayer().getCurrentPokemon());
         pokemonsSplitPane.getChildren().add(currentPokemonView);
+    }
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) throws IOException {
+        try {
+            gameAPI.start();
+            changeScene(event, getResource("views/pokemon-choice.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
