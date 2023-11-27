944f31ee4e1192a29dbd0ab1d37c1d0a9f2e909c
Pablo Gonzalez <pdgonzalez@fi.uba.ar>
2023-11-27 02:42:40 -0300

    fix error json.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -22,4 +22,7 @@ module org.fiuba.algoritmos3 {
     opens org.fiuba.algoritmos3.view to javafx.fxml;
     exports org.fiuba.algoritmos3.view.chooseGameMove;
     opens org.fiuba.algoritmos3.view.chooseGameMove to javafx.fxml;
+
+    exports org.fiuba.algoritmos3.model;
+
 }
