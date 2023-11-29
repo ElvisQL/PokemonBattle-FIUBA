18be24eb8bdf4d1a1b63f6ad6f97b3726c97ced4
tcorzo <t.corzo.personal@gmail.com>
2023-11-29 17:20:12 -0300

    Removed dead pokemons from change pokemon

--- src/main/java/org/fiuba/algoritmos3/controller/gameMove/ChangePokemonController.java
+++ src/main/java/org/fiuba/algoritmos3/controller/gameMove/ChangePokemonController.java
@@ -8,6 +8,7 @@ import org.fiuba.algoritmos3.model.move.builder.ChangePokemonBuilder;
 import org.fiuba.algoritmos3.model.pokemon.Pokemon;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ChangePokemonController extends GameMoveController<ChangePokemon, ChangePokemonBuilder> {
@@ -15,7 +16,12 @@ public class ChangePokemonController extends GameMoveController<ChangePokemon, C
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         builder = new ChangePokemonBuilder();
-        PickerController<Pokemon> controller = loadPicker(PokemonPickerController.class, gameAPI.currentPlayer().getPokemons());
+
+        List<Pokemon> availablePokemons = gameAPI.currentPlayer().getPokemons().stream()
+                .filter((pokemon -> !pokemon.isDead()))
+                .toList();
+        PickerController<Pokemon> controller = loadPicker(PokemonPickerController.class, availablePokemons);
+
         controller.addSelectionListener(this::onPokemonPicked);
         controller.addBackListener((_a, _b, _c) -> loadChooseGameMove());
         controller.setFilterFunction((pokemon -> !pokemon.isDead()));
