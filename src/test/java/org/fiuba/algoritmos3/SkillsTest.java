2fce377e9fa258ae237faeb4399499e0d5a588b7
Pablo Gonzalez <pdgonzalez@fi.uba.ar>
2023-11-30 01:10:37 -0300

    fix

--- src/test/java/org/fiuba/algoritmos3/SkillsTest.java
+++ src/test/java/org/fiuba/algoritmos3/SkillsTest.java
@@ -1,5 +1,6 @@
 package org.fiuba.algoritmos3;
 
+import org.fiuba.algoritmos3.model.GameState;
 import org.fiuba.algoritmos3.model.move.errors.NoRemainingUsesError;
 import org.fiuba.algoritmos3.model.pokemon.Pokemon;
 import org.fiuba.algoritmos3.model.pokemon.skills.AttackSkill;
@@ -9,13 +10,19 @@ import org.fiuba.algoritmos3.model.pokemon.skills.StatusSkill;
 import org.fiuba.algoritmos3.model.pokemon.status.ParalyzedStatus;
 import org.fiuba.algoritmos3.model.pokemon.status.Status;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class SkillsTest {
 
+    @Mock
+    GameState gameState;
     //AttackSKill
     @Test
     void testApplySkillNoRemainingUses() {
@@ -24,7 +31,7 @@ public class SkillsTest {
 
         AttackSkill attackSkill = new AttackSkill("Quick Attack", 20, 0, "A quick attack");
 
-        assertThrows(NoRemainingUsesError.class, () -> attackSkill.apply(attacker, target));
+        assertThrows(NoRemainingUsesError.class, () -> attackSkill.apply(attacker, target,gameState));
     }
 
 
@@ -38,9 +45,9 @@ public class SkillsTest {
 
         int expectedAttackIncrease = 10;
 
-        BuffSkill buffSkill = new BuffSkill("Attack Boost", StatType.ATTACK, expectedAttackIncrease, "Boosts attack");
+        BuffSkill buffSkill = new BuffSkill("Attack Boost", StatType.ATTACK, expectedAttackIncrease, "Boosts attack",1);
 
-        buffSkill.apply(charizard, null);
+        buffSkill.apply(charizard, null,gameState);
 
         verify(charizard, times(1)).setAttackPoints(initialAttackPoints + expectedAttackIncrease);
         assertTrue(charizard.getAttackPoints() >= initialAttackPoints);
@@ -55,9 +62,9 @@ public class SkillsTest {
 
         int expectedAttackIncrease = 10;
 
-        BuffSkill buffSkill = new BuffSkill("Health Boost", StatType.HEALTH, expectedAttackIncrease, "Boosts attack");
+        BuffSkill buffSkill = new BuffSkill("Health Boost", StatType.HEALTH, expectedAttackIncrease, "Boosts attack",1);
 
-        buffSkill.apply(charizard, null);
+        buffSkill.apply(charizard, null,gameState);
 
         verify(charizard, times(1)).setHealth(initialAttackPoints + expectedAttackIncrease);
         assertTrue(charizard.getHealth() >= initialAttackPoints);
@@ -72,9 +79,9 @@ public class SkillsTest {
 
         int expectedAttackIncrease = 5;
 
-        BuffSkill buffSkill = new BuffSkill("Speed Boost", StatType.SPEED, expectedAttackIncrease, "Boosts Speed");
+        BuffSkill buffSkill = new BuffSkill("Speed Boost", StatType.SPEED, expectedAttackIncrease, "Boosts Speed",1);
 
-        buffSkill.apply(charizard, null);
+        buffSkill.apply(charizard, null,gameState);
 
         verify(charizard, times(1)).setAttackSpeed(initialAttackPoints + expectedAttackIncrease);
         assertTrue(charizard.getAttackSpeed() >= initialAttackPoints);
@@ -89,9 +96,9 @@ public class SkillsTest {
 
         int expectedAttackIncrease = 10;
 
-        BuffSkill buffSkill = new BuffSkill("Defense Boost", StatType.DEFENSE, expectedAttackIncrease, "Boosts Defense");
+        BuffSkill buffSkill = new BuffSkill("Defense Boost", StatType.DEFENSE, expectedAttackIncrease, "Boosts Defense",1);
 
-        buffSkill.apply(charizard, null);
+        buffSkill.apply(charizard, null,gameState);
 
         verify(charizard, times(1)).setDefencePoints(initialAttackSpeed + expectedAttackIncrease);
         assertTrue(charizard.getDefencePoints() >= initialAttackSpeed);
@@ -105,11 +112,13 @@ public class SkillsTest {
         Pokemon otherPokemon = mock(Pokemon.class);
         Status expectedStatus = mock(ParalyzedStatus.class);
 
-        StatusSkill statusSkill = new StatusSkill("Paralyze", expectedStatus, "Paralyzes the opponent");
+        StatusSkill statusSkill = new StatusSkill("Paralyze", expectedStatus, "Paralyzes the opponent",1);
 
-        statusSkill.apply(pokemon, otherPokemon);
+        statusSkill.apply(pokemon, otherPokemon,gameState);
 
         verify(otherPokemon, times(1)).addStatus(expectedStatus);
     }
+
+
 
 }
